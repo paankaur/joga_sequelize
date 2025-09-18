f54d1552e63078f1481801061921c1d54aedb066
Paan Kaur <paan.kaur@gmail.com>
2025-09-18 13:48:38 +0300

    Create connection to database

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const PORT = 3026;
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Hello from Yoga Sequelize!');
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_PASSWORDDB_HOST,
+    dialect: "mysql",
+  }
+);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established sucasdcessfully.");
+  } catch (err) {
+    console.error("Unable to connect to the database:", err);
+  }
+})();
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Sequelize example!" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
